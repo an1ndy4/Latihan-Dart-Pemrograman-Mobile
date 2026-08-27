74294d0f68e4d5e5169e8d15924a4cde8ae84dcc
Nur Cahya Ningdiah - 089 <nurcahyaningdiah2@gmail.com>
2026-08-27 13:43:44 +0900

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,12 @@
+void main() {
+    String nama = "nur cahya ningdiah";
+    int umur = 20;
+    double berat = 40.5;
+    num tinggi = 150;
+
+    print("========== data diri ==========");
+    print("Nama: $nama");
+    print("Umur: $umur");
+    print("Berat: $berat");
+    print("Tinggi: $tinggi");
+}
---
+++ main2.dart
@@ -0,0 +1,8 @@
+void main() {
+    int perkalian(int x, int y) {
+        return x * y;
+    }
+
+    // perkalian(10,20)
+    print(perkalian(10, 20));
+}
